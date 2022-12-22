0e3ff52c5aee62b1bceebc10846bff47353bfba9
Seohyun Hong <96035605+Hongseohyun@users.noreply.github.com>
2022-12-22 18:28:06 +0900

    Add files via upload

---
+++ 2022_2 final/problem.js
@@ -0,0 +1,44 @@
+let p1 = {
+        
+}
+
+let problems = {
+    prblib: [
+  {   
+    kor: "넌 어떻게 하는지 알잖아",
+    eng: "you know the drill"   
+   }
+,
+  {kor: "우여곡절이 많았어",
+  eng : "It's been a roller coaster ride"},
+
+ {kor: "난 이런저런 것들을 들었어",
+  eng: "I've heard bits and bobs"},
+
+ {kor: "당신을 존경해요",
+  eng: "I look up to you"},
+
+ {kor: "그건 과대평가된 것 같아",
+  eng: "I think it's overrated"},
+
+ {kor: "그것은 전화위복이었어",
+  eng: "It was a blessing in disguise"},
+
+ {kor: "난 뒤끝이 없어",
+  eng: "I don't hold a grudge"},
+
+ {kor: "큰일 날 뻔했어",
+  eng: "that was a close call"},
+
+ {kor: "최선의 아이디어는 아니야",
+  eng: "It is not the best idea"},
+
+ {kor: "나 그거 지긋지긋해",
+  eng: "I'm fed up with it"}
+]
+}
+  
+
+
+
+// }
